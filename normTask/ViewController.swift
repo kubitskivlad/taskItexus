104666840b0cf2cb2f96b62666b2221d1f212ab4
it0046 <it0046@it0046s-Mac-mini.local>
2018-12-20 13:30:17 +0300

    Initial Commit

---
+++ normTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  normTask
+//
+//  Created by it0046 on 12/20/18.
+//  Copyright © 2018 it0046. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
